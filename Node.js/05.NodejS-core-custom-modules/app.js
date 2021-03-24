dcdeaff9aaacddd8cb4d6ab9b89437b8aadbba91
Neelam Rana <63229573+Neelam092@users.noreply.github.com>
2021-03-24 09:05:59 +0530

    Add files via upload

--- Node.js/05.NodejS-core-custom-modules/app.js
+++ Node.js/05.NodejS-core-custom-modules/app.js
@@ -1,1 +1,9 @@
+const store = require('./database/store/dataStore');
+const util = require('./util/utility');
 
+// call function
+store.getAllEmployees((employees)=>{
+    console.log(employees);
+    let fileName = `customers.json`;
+    util.writeToFile(fileName,JSON.stringify(employees));
+});
