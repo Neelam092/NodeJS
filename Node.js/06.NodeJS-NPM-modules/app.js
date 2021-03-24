8584cd8fa5bcbc6f59de3cea591c92a8887f5110
Neelam Rana <63229573+Neelam092@users.noreply.github.com>
2021-03-24 09:10:32 +0530

    Add files via upload

--- Node.js/06.NodeJS-NPM-modules/app.js
+++ Node.js/06.NodeJS-NPM-modules/app.js
@@ -1,1 +1,24 @@
+const bcrypt = require('bcryptjs');
 
+let user ={
+    name : 'John',
+    email : 'jogn@gmail.com',
+    password : 'john@123'
+}
+let salt = bcrypt.genSaltSync(10);
+let hashedPassword = bcrypt.hashSync(user.password,salt);
+//console.log(hashedPassword);
+
+let updatedUser = {
+    ...user,
+    password: hashedPassword
+};
+//console.log(updatedUser);
+
+// compare both passwords
+if(bcrypt.compareSync('john@123',hashedPassword)){
+    console.log('Password is matched , You are in');
+}
+else{
+    console.log('Password is not matched, You are out');
+}
