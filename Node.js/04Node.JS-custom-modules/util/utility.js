8f160fd9edcad475ca2928490e20a97c72444683
Neelam Rana <63229573+Neelam092@users.noreply.github.com>
2021-03-24 08:49:52 +0530

    Add files via upload

--- Node.js/04Node.JS-custom-modules/util/utility.js
+++ Node.js/04Node.JS-custom-modules/util/utility.js
@@ -1,1 +1,18 @@
+// declare a function
+ let greet = (name)=>{
+    console.log(`Hello! ${name} `);
+}
 
+// reverse string
+let convertReverse =(str)=>{
+    let temp = '';
+    for(let i = str.length-1; i>=0;i--){
+        temp += str.charAt(i);
+    }
+    return temp;
+}
+// export function
+module.exports = {
+    greet,
+    convertReverse
+};
