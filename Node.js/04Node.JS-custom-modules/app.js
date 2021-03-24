66aac0f919dec180501219e2d1e352e1fa6fba83
Neelam Rana <63229573+Neelam092@users.noreply.github.com>
2021-03-24 08:48:22 +0530

    Add files via upload

--- Node.js/04Node.JS-custom-modules/app.js
+++ Node.js/04Node.JS-custom-modules/app.js
@@ -1,1 +1,7 @@
+const util = require('./util/utility');
 
+//calla fnction
+util.greet('Naveen');
+let message = 'Hello';
+let revString = util.convertReverse(message);
+console.log(`${message} -> ${revString}`);
