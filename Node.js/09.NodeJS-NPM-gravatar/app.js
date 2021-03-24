8334949e4a2c1eb9848e249d7cf407dfedd63368
Neelam Rana <63229573+Neelam092@users.noreply.github.com>
2021-03-24 09:17:19 +0530

    Add files via upload

--- Node.js/09.NodeJS-NPM-gravatar/app.js
+++ Node.js/09.NodeJS-NPM-gravatar/app.js
@@ -1,1 +1,20 @@
+const avatar = require('gravatar');
+
+let user ={
+    name : 'Neelam Rana',
+    email : 'neelamrana092@gmail.com',
+    password : '12345'
+};
+
+let avatarURL = avatar.url(user.email , {
+    s : 400,
+    r : 'pg',
+    d : 'mm'
+});
+
+let updatedUser = {
+    ...user,
+    avatar : avatarURL
+};
+console.log(updatedUser);
 
