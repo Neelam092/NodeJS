c866425aa0f8a94cf521d687335f68f2945055e6
Neelam Rana <63229573+Neelam092@users.noreply.github.com>
2021-03-24 08:30:53 +0530

    Add files via upload

--- Node.js/NodeJS-Basics/app.js
+++ Node.js/NodeJS-Basics/app.js
@@ -1,1 +1,21 @@
+let message = 'Good Morning';
+console.log(message);
 
+//object
+let mobile = {
+    brand : 'Apple',
+    price : 35000,
+    color : 'silver;'
+};
+console.log(mobile);
+
+// Array
+let technology = ['JavaScript','Html','CSS','NodeJs','ReactJs'];
+console.log(technology);
+
+// Function
+let greet = (name)=>{
+    let message = `Hello ${name}`;
+    console.log(name);
+}
+greet('John');
