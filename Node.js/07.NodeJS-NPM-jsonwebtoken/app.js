25f4b6e99025b5f3c088e48b0013910789e0b050
Neelam Rana <63229573+Neelam092@users.noreply.github.com>
2021-03-24 09:12:21 +0530

    Add files via upload

--- Node.js/07.NodeJS-NPM-jsonwebtoken/app.js
+++ Node.js/07.NodeJS-NPM-jsonwebtoken/app.js
@@ -1,1 +1,23 @@
+const jwt = require('jsonwebtoken');
 
+let user = {
+    id : 'abcd-001-sfsd-2399',
+    name : 'john',
+    email : 'john@gmail.com'
+};
+
+let payload ={
+    id : user.id,
+    name : user.name
+};
+let secretKey = 'ssshhhh';
+
+// generate new token
+let token = jwt.sign(payload, secretKey, {expiresIn : 3600});
+console.log(token);
+
+// verify token
+jwt.verify(token, secretKey,(err, decoded )=>{
+    if(err)throw err;
+    console.log(decoded);
+});
