12d5af51feaa2924c0a657449254cba5d5c7307c
Neelam Rana <63229573+Neelam092@users.noreply.github.com>
2021-03-24 09:14:06 +0530

    Add files via upload

--- Node.js/08.NodeJS-NPM-dotenv/app.js
+++ Node.js/08.NodeJS-NPM-dotenv/app.js
@@ -1,1 +1,11 @@
+const dotenv = require('dotenv');
 
+//configure the config file
+dotenv.config({path : './config/config.env'});
+
+// to read data from the env file
+let devURL = process.env.DEV_URL;
+console.log(`DEV_URL : ${devURL}`);
+
+let mongoLocal = process.env.MONGO_DB_CLOUD;
+console.log(`MongoDB Local : ${mongoLocal}`);
